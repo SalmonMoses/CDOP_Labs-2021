a2a7be53d6f24901e8ee2c2f3a46d046d195ded1
Mikhail Melamed <mikhail.melamed@gmail.com>
2021-09-23 17:13:22 +0300

    Added remove button to lab 3

--- lab3/src/app/app.component.ts
+++ lab3/src/app/app.component.ts
@@ -14,7 +14,17 @@ export class AppComponent {
   }
 
   get sumPrice(): number {
-    if(this.products.length < 1) return 0;
+    if (this.products.length < 1) return 0;
     return this.products.map(entry => entry.price).reduce((acc, val) => acc + val);
+  }
+
+  deleteProduct(index: number) {
+    if (index === 0) {
+      this.products.shift();
+    } else if (index === this.products.length - 1) {
+      this.products.pop();
+    } else {
+      this.products.splice(index, 1);
+    }
   }
 }
